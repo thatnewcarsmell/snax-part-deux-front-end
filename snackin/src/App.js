629592a1ff1e39444638c1bec609d38445edd620
zipit145 <gregbforney@gmail.com>
2018-12-07 12:39:54 -0700

    added login functionality forms

--- snackin/src/App.js
+++ snackin/src/App.js
@@ -5,6 +5,7 @@ import Snacklist from './components/Snacklist'
 import Footer from  './components/Footer'
 import Snack from './components/Snack'
 
+
 class App extends Component {
   
   constructor (props) {
@@ -23,6 +24,9 @@ class App extends Component {
   //   })
   //   console.log(this.state.email)
   // }
+  modalListener = () => {
+
+  }
   render() {
     return (
       <div className="App">
@@ -31,8 +35,11 @@ class App extends Component {
       <Snacklist data={this.state.data}/>
       <Snack/>
       <Footer/>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
       </div>
+      
     );
   }
 }
--- snackin/src/components/Navbar.js
+++ snackin/src/components/Navbar.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import Login from './Login'
 
-const Navbar = () => {
+const Navbar = (props) => {
     return (
       <nav class="navbar navbar-light bg-secondary">
         <a class="navbar-brand" href="#">
           <img src="https://github.com/ninjames101/snax-part-deux/blob/master/snacks_logo.png?raw=true" height="35em" alt="logo"/>
         </a>
-        <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Login</button>
+        <button type="button" class="btn btn-outline-light my-2 my-sm-0" data-toggle="modal" data-target="#login">
+            Log in
+        </button>
+        <button type="button" class="btn btn-outline-light my-2 my-sm-0" data-toggle="modal" data-target="#signup">
+            Sign up
+        </button>
+        <Login />
       </nav>
     )
   }
